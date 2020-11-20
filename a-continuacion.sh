45bbf7d9ed36198fa998865f8082c0ab93c66ee5
Ramiro <64646831+rraiomgg@users.noreply.github.com>
2020-11-20 12:22:44 -0700

    Create a-continuacion.sh

---
+++ a-continuacion.sh
@@ -0,0 +1,9 @@
+ln -sf /usr/share/zoneinfo/America/Mazatlan /etc/localtime
+hwclock --systohc
+nano /etc/locale.gen
+locale-gen 
+echo "LANG=es_MX.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=la-latin1" >> /etc/vconsole.conf
+cp ./hostname /etc/hostname
+cp ./hosts /etc/hosts
+pacman -S grub ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober alsa-firmware mtools dosfstools base-devel linux-lts-headers bluez cups pulseaudio xdg-utils xdg-user-dirs xf86-video-intel nodejs npm code jdk-openjdk xorg gdm gnome-control-center budgie-desktop gnome-terminal plank
