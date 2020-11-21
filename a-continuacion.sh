21f85cf7404f0b9dd7e2a7ee10c4b7050c31b079
Ramiro <64646831+rraiomgg@users.noreply.github.com>
2020-11-21 13:28:04 -0700

    Update a-continuacion.sh

--- a-continuacion.sh
+++ a-continuacion.sh
@@ -6,4 +6,4 @@ echo "LANG=es_MX.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=la-latin1" >> /etc/vconsole.conf
 cp ./hostname /etc/hostname
 cp ./hosts /etc/hosts
-pacman -S grub ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober alsa-firmware mtools dosfstools base-devel linux-lts-headers bluez cups pulseaudio xdg-utils xdg-user-dirs xf86-video-intel nodejs npm code jdk-openjdk xorg gdm xfce4
+pacman -S grub ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober alsa-firmware mtools dosfstools base-devel linux-lts-headers bluez cups pulseaudio xdg-utils xdg-user-dirs xf86-video-intel nodejs npm code jdk-openjdk xorg lxdm xfce4
