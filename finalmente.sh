af19cc476385c530c0e4db2c10cea046671b677c
Ramiro <64646831+rraiomgg@users.noreply.github.com>
2020-11-21 13:28:45 -0700

    Update finalmente.sh

--- finalmente.sh
+++ finalmente.sh
@@ -3,7 +3,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable org.cups.cupsd
-systemctl enable gdm
+systemctl enable lxdm.service
 passwd
 useradd -mG wheel ramiro
 passwd ramiro
