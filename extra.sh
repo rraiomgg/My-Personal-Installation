6f8a201b4f19138d5a25f3aedcf373093f6d207c
Ramiro <64646831+rraiomgg@users.noreply.github.com>
2020-12-13 21:12:53 -0700

    Update extra.sh

--- extra.sh
+++ extra.sh
@@ -1,11 +1,8 @@
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si PKGBUILD
-    yay -S nvidia-390xx
+    yay -S nvidia-390xx-dkms
     yay -S google-chrome
-    yay -S xampp
-    sudo npm install -g n
-    sudo n 14.0.0
     sudo npm install -g expo-cli
     git clone https://aur.archlinux.org/aic94xx-firmware.git
     cd aic94xx-firmware
@@ -15,3 +12,5 @@
     cd wd719x-firmware
     makepkg -sri
     sudo mkinitcpio -p linux-lts
+    sudo pacman -S virtualbox-host-dkms
+    sudo modprobe vboxdrv
