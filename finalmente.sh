404fbccd9e2d17381714a473edacbd063ea85e56
Ramiro <64646831+rraiomgg@users.noreply.github.com>
2020-12-13 21:06:28 -0700

    Update finalmente.sh

--- finalmente.sh
+++ finalmente.sh
@@ -1,9 +1,8 @@
-grub-install --target=i386-pc /dev/device
+grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable bluetooth
-systemctl enable org.cups.cupsd
-systemctl enable lxdm.service
+systemctl enable gdm
 passwd
 useradd -mG wheel ramiro
 passwd ramiro
